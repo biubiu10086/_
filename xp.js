f3794d829710011faa16ce90d6e6b8e9a9e583db
Lexho1 <62320295+Lexho1@users.noreply.github.com>
2021-01-17 02:00:08 +0800

    Update xp.js

--- xp.js
+++ xp.js
@@ -22,7 +22,7 @@ const nowTimes = new Date(
 );
 // 没有设置 XP_CASH 则默认为 0 不提现
 if ($.isNode()) {
- CASH = process.env.XP_CASH || 0;
+ CASH = process.env.XP_CASH || 1;
 } 
 if ($.isNode() && process.env.XP_iboxpayHEADER) {
   COOKIES_SPLIT = process.env.COOKIES_SPLIT || "\n";
