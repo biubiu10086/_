d67f329e3c519827173c47e0eebd5148fdf1224f
Lexho1 <62320295+Lexho1@users.noreply.github.com>
2021-01-17 18:54:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ exports.main_handler = async (event, context, callback) => {
       //})
 
       //3.执行github远端的js文件(因github的raw类型的文件被墙,此方法云函数不推荐)
-      request('https://github.com/ziye12/JavaScript/tree/main/Task/' + v + '.js', function (error, response, body) {
+      request('https://github.com/ZhiYi-N/Private-Script/master/Scripts/' + v + '.js', function (error, response, body) {
        eval(response.body)
       })
     }
