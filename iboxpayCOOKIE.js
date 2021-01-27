dabaf0548b5ff3a72cfba2c18d29d6c204cb2c53
Lexho1 <62320295+Lexho1@users.noreply.github.com>
2021-01-27 11:05:05 +0800

    Update iboxpayCOOKIE.js

--- iboxpayCOOKIE.js
+++ iboxpayCOOKIE.js
@@ -1,5 +1,5 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let iboxpayheaderVal= ``
+let iboxpayheaderVal= `{"Connection":"keep-alive","Accept-Encoding":"gzip, deflate, br","version":"1.4.4","mchtNo":"100529600058887","Content-Type":"application/json; charset=utf-8","source":"VEISHOP_APP_IOS","shopkeeperId":"1148855820752977920","User-Agent":"VeiShop, 1.4.4 (iOS, 14.3, zh_CN, Apple, iPhone, 1CC223A6-BB53-47A6-9091-AF666380AF50)","token":"477d38c1aeea438cb4bad3c7291bcc01","X-User-Agent":"VeiShop, 1.4.4 (iOS, 14.3, zh_CN, Apple, iPhone, 1CC223A6-BB53-47A6-9091-AF666380AF50)","traceid":"3134878986933678080016117157081802bfa26fca4b8","Host":"veishop.iboxpay.com","Accept-Language":"zh-Hans;q=1","Accept":"*/*"}`
 
 
 
