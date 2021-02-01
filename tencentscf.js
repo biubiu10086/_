bb356a8cf406b21b11b12134942fbf206721b036
Lexho1 <62320295+Lexho1@users.noreply.github.com>
2021-02-01 08:13:27 +0800

    Update tencentscf.js

--- tencentscf.js
+++ tencentscf.js
@@ -10,7 +10,7 @@ const clientConfig = {
     secretId: process.env.TENCENT_SECRET_ID,
     secretKey: process.env.TENCENT_SECRET_KEY,
   },
-  region: process.env.TENCENT_REGION, // 区域参考，https://cloud.tencent.com/document/product/583/17299
+  region: process.env.TENCENT_REGION, // 区域参考，https://cloud.tencent.com/document/product/213/6091
   profile: {
     httpProfile: {
       endpoint: "scf.tencentcloudapi.com",
@@ -60,8 +60,9 @@ const sleep = ms => new Promise(res => setTimeout(res, ms));
             console.error("error", err);
           }
         );
-        await sleep(1000*100) // 等待100秒
+        // await sleep(1000*100) // 等待100秒
       }
+      await sleep(1000*100) // 等待100秒
     },
     (err) => {
       console.error("error", err);
@@ -72,14 +73,83 @@ const sleep = ms => new Promise(res => setTimeout(res, ms));
   // 更新环境变量
   let inputYML = '.github/workflows/deploy_tencent_scf.yml';
   let obj = yaml.load(fs.readFileSync(inputYML, {encoding: 'utf-8'}))
+
+  let L_env = [
+    "JD_COOKIE",
+    "JD_DEBUG",
+    "JD_USER_AGENT",
+    "PUSH_KEY",
+    "BARK_PUSH",
+    "BARK_SOUND",
+    "TG_BOT_TOKEN",
+    "TG_USER_ID",
+    "DD_BOT_TOKEN",
+    "DD_BOT_SECRET",
+    "QYWX_KEY",
+    "IGOT_PUSH_KEY",
+    "QQ_SKEY",
+    "QQ_MODE",
+    "PUSH_PLUS_TOKEN",
+    "PUSH_PLUS_USER",
+    "TG_PROXY_HOST",
+    "TG_PROXY_PORT",
+    "FRUITSHARECODES",
+    "PETSHARECODES",
+    "PLANT_BEAN_SHARECODES",
+    "SUPERMARKET_SHARECODES",
+    "DDFACTORY_SHARECODES",
+    "DREAM_FACTORY_SHARE_CODES",
+    "JDZZ_SHARECODES",
+    "JDJOY_SHARECODES",
+    "BOOKSHOP_SHARECODES",
+    "JD_CASH_SHARECODES",
+    "JXNC_SHARECODES",
+    "JDNIAN_SHARECODES",
+    "JDSXSY_SHARECODES",
+    "JD_BEAN_STOP",
+    "JD_BEAN_SIGN_STOP_NOTIFY",
+    "JD_BEAN_SIGN_NOTIFY_SIMPLE",
+    "PET_NOTIFY_CONTROL",
+    "FRUIT_NOTIFY_CONTROL",
+    "JD_JOY_REWARD_NOTIFY",
+    "JOY_FEED_COUNT",
+    "JOY_HELP_FEED",
+    "JOY_RUN_FLAG",
+    "JOY_TEAM_LEVEL",
+    "JOY_RUN_NOTIFY",
+    "JD_JOY_REWARD_NAME",
+    "MARKET_COIN_TO_BEANS",
+    "MARKET_REWARD_NOTIFY",
+    "JOIN_PK_TEAM",
+    "SUPERMARKET_UPGRADE",
+    "BUSINESS_CIRCLE_JUMP",
+    "SUPERMARKET_LOTTERY",
+    "FRUIT_BEAN_CARD",
+    "UN_SUBSCRIBES",
+    "UN_BIND_CARD_NUM",
+    "UN_BIND_STOP_CARD",
+    "JDJOY_HELPSELF",
+    "JDJOY_APPLYJDBEAN",
+    "BUY_JOY_LEVEL",
+    "MONEY_TREE_SELL_FRUIT",
+    "FACTORAY_WANTPRODUCT_NAME",
+    "JXNCTOKENS",
+    "JD_IMMORTAL_LATLON",
+    "JD_WECHAT_USER_AGENT",
+    "DREAMFACTORY_FORBID_ACCOUNT",
+    "JDFACTORY_FORBID_ACCOUNT"
+  ] 
+
   let vars = []
-  for(let key in obj.jobs.build.steps[3].env){
-    if(key!=='PATH' && process.env.hasOwnProperty(key))
+  for(let key in process.env){
+    if(process.env[key]!=='' && L_env.includes(key))
       vars.push({
         "Key": key,
         "Value": process.env[key]
-      })
+      })    
+      // console.log(`in L_env is=${key}:${process.env[key]}`)  //debug
   }
+
   console.log(`您一共填写了${vars.length}个环境变量`)
   params = {
     "FunctionName": process.env.TENCENT_FUNCTION_NAME,
