339e5170babc4aaeff2a551a2328c6d2e7dc7c4d
Lexho1 <62320295+Lexho1@users.noreply.github.com>
2021-02-28 19:19:38 +0800

    Create flwCOOKIE.js

---
+++ flwCOOKIE.js
@@ -0,0 +1,17 @@
+let flwurlVal= ``
+let flwheaderVal= ``
+let flwspbodyVal= ``
+let flwqwbodyVal= ``
+let flwydbodyVal= ``
+
+
+let flwcookie = {
+  flwurlVal: flwurlVal,  
+  flwheaderVal: flwheaderVal,  
+  flwspbodyVal: flwspbodyVal, 
+  flwqwbodyVal: flwqwbodyVal, 
+  flwydbodyVal: flwydbodyVal, 
+
+}
+
+module.exports =  flwcookie
